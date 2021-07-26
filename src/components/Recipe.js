69f26e898dbc68b0f8297bad373ae32458ec3d37
“Jamie <knutsenjamie@yahoo.com>
2021-07-26 15:04:38 -0700

    begin building out PropTypes and adding img src to Recipe.js and RecipeController.js

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,14 +1,28 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 function Recipe(props) {
     return (
-        <h1>hello</h1>
+    <React.Fragment>
+        <img src={props.recipePicture} alt='recipe photo' className='recipePicture'></img>
+        <h1>{props.name}</h1>
+        <h4>{props.category}</h4>
+        <h4>{props.cuisine}</h4>
+        
+    </React.Fragment>
     )
 }
 
 Recipe.propTypes = {
-
+recipePicture: PropTypes.string,
+name: PropTypes.string,
+category: PropTypes.string,
+cuisine: PropTypes.string,
+ingredients: PropTypes.string,
+time: PropTypes.string,
+yield: PropTypes.number,
+instructions: PropTypes.string,
+recipeId: PropTypes.number
 }
 
 export default Recipe; 
