3ec091e897c70b149bad4802d1ea9e91a2f5274f
“Jamie <knutsenjamie@yahoo.com>
2021-07-26 13:59:42 -0700

    change aApp.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import Header from './Header';
+import RecipeControl from './RecipeControl';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <RecipeControl />
+    </>
+  );
+}
+
+export default App;
