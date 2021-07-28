e0eddee73d170e6b4e0921f073dc853af3a4252c
“Jamie <knutsenjamie@yahoo.com>
2021-07-28 15:06:16 -0700

    add logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Header() {
   return (
-    <h1>Welcome To Vitally Veg 2.0</h1>
+    <React.Fragment>
+    <h1>Welcome To Vitally Veg 2.0<h1>
+      
+    </h1>
+    <img src='https://vitallyveg.com/wp-content/uploads/2020/01/cropped-vitally-veg-logo-1.png' width='200px' alt='vitally veg logo'></img>
+    </h1>
+    </React.Fragment>
   )
 }
 
