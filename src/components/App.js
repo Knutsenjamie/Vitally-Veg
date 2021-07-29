1363975aa81cfbd336acf8904a135f956580bc7d
“Jamie <knutsenjamie@yahoo.com>
2021-07-28 21:43:51 -0700

    add Footer.js and dummy button

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css';
 import Header from './Header';
+import Footer from './Footer';
 import RecipeControl from './RecipeControl';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <RecipeControl />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <React.Fragment>
+    <button className='btn btn-success'>Make Grocery List</button>
+    </React.Fragment>
+  )
+}
+export default Footer;
