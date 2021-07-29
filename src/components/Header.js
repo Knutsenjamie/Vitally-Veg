b16974520e0b6234fba9364be233f6a0873d213f
“Jamie <knutsenjamie@yahoo.com>
2021-07-28 20:45:40 -0700

    add recipeCard to App.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <React.Fragment>
-    <h1>Welcome To Vitally Veg 2.0<h1>
-      
+    <h1><strong>Welcome To Vitally Veg 2.0</strong><h1>
     </h1>
     <img src='https://vitallyveg.com/wp-content/uploads/2020/01/cropped-vitally-veg-logo-1.png' width='200px' alt='vitally veg logo'></img>
     </h1>
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -7,13 +7,15 @@ function Recipe(props) {
     return (
     <React.Fragment>
         <img src={props.recipeImage} alt='recipe photo' className='recipePicture'></img>
-        <h1>{props.name}</h1>
+        <h2>{props.name}</h2>
         <h3><strong>Category:</strong> {props.category}</h3>
         <h3><strong>Cuisine:</strong> {props.cuisine}</h3>
         <h4><strong>Ingredients:</strong> {props.ingredients}</h4>
         <h4><strong>Total Recipe Time:</strong> {props.time}</h4>
         <h4><strong>Yields:</strong> {props.makes}</h4>
+    <div className ='recipeCard'>
         <h4><strong>Instructions:</strong> {props.instructions}</h4>  
+    </div>
     </React.Fragment>
     )
 }
